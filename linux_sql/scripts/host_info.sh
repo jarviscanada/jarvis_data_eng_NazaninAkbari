19e0edd6619cfe2333727062abf1ea69785567f2
NazaninAkbari <71785631+Na-zi@users.noreply.github.com>
2021-12-07 18:52:15 -0500

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -10,21 +10,19 @@ then
   exit 1
 fi
 lscpu_out=`lscpu`
-hostname=$(hostname -f)
-cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3,$4,$5,$6,$7}' | xargs)
-cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
-total_mem=$(free | awk 'NR==2 {print $2 }' | xargs)
-timestamp=$(vmstat -t |awk '{print $18,$19}'| tail -1)
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+hostname="$(hostname -f)"
+lscpu_out="$(lscpu)"
 
-insert_stmt="INSERT INTO host_info
-(id,hostname,cpu_number,cpu_architecture,cpu_model,cpu_mhz,L2_cache,timestamp)
- VALUES
-('$hostname','$cpu_number','$cpu_architecture','$cpu_model','$cpu_mhz','$L2_cache','$timestamp')"
+#These instructions retrieves the hardware specifications.
+cpu_number="$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)"
+cpu_architecture="$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)"
+cpu_model="$(echo "$lscpu_out" | egrep "^Model\sname:" | awk '{print $3,$4,$5,$6,$7}' | xargs)"
+cpu_mhz="$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)"
+l2_cache="$(echo "$lscpu_out" | grep '^L2 cache' | cut -f 2 -d ":" | awk '{$1=$1}1' | cut -f 1 -d "K")"
+total_mem="$(grep MemTotal /proc/meminfo | awk '{print $2}')"
+timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
 
-export PGPASSWORD=$psql_password
-psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
-exit $?
+#inserts the hardware specification to the host_info table
+insert_statement="insert into host_info (hostname,cpu_number,cpu_architecture,cpu_model,cpu_mhz,l2_cache,total_mem,timestamp)values('$hostname',$cpu_number,'$cpu_architecture','$cpu_model','$cpu_mhz','$l2_cache',$total_mem,'$timestamp')"
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_statement"
+
