b663dd1337fe549b002aa812beae552e4d56d93f
NazaninAkbari <71785631+Na-zi@users.noreply.github.com>
2021-12-08 12:42:53 -0500

    Update ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,28 +1,23 @@
-\c host_agent;
-CREATE TABLE IF NOT EXISTS PUBLIC.host_info
-  (
-     id               SERIAL NOT NULL, 
-     hostname         VARCHAR NOT NULL,
-     cpu_number        INT NOT NULL,
-     cpu_architecture  VARCHAR NOT NULL,
-     cpu_model         VARCHAR NOT NULL,
-     cpu_mhz           FLOAT(3) NOT NULL,
-     L2_cache           INT NOT NULL,
-     total_mem   INT NOT NULL,
-     "timestamp"  TIMESTAMP NOT NULL,
-     PRIMARY KEY(id),
-     UNIQUE(hostname)
+\c host_agent
+CREATE TABLE if not exists PUBLIC.host_info (
+    id SERIAL primary key NOT NULL,
+    hostname VARCHAR NOT NULL UNIQUE,
+    cpu_number integer not null,
+    cpu_architecture varchar not null,
+    cpu_model varchar not null,
+    cpu_mhz real not null,
+    l2_cache integer not null,
+    total_mem integer not null,
+    "timestamp" timestamptz not null
   );
-CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
-  (
-     "timestamp"  TIMESTAMP NOT NULL,
-     host_id        SERIAL NOT NULL,
-     memory_free     INT NOT NULL,
-     cpu_idle      DECIMAL NOT NULL,
-     disk_io       INT NOT NULL,
-     disk_available   INT NOT NULL,
-     CHECK (cpu_idle BETWEEN 0 AND 100),
-     CHECK (cpu_kernel BETWEEN 0 AND 100),
-     FOREIGN KEY (host_id) REFERENCES host_info(id)
+CREATE TABLE if not exists PUBLIC.host_usage (
+    "timestamp" TIMESTAMP NOT NULL,
+    host_id int NOT NULL,
+    memory_free integer not null,
+    cpu_idle integer not null,
+    cpu_kernel integer not null,
+    disk_io integer not null,
+    disk_available integer not null,
+    CONSTRAINT foreign_host_id FOREIGN KEY(host_id) REFERENCES host_info(id)
 );
 
