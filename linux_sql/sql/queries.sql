3e343323a31e0a1ecfa3724843a23241c4fef98e
NazaninAkbari <71785631+Na-zi@users.noreply.github.com>
2021-12-16 13:59:10 -0500

    Update queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,33 +1,43 @@
 -- Group hosts by hardware info
-select
-    cpu_number, id as host_id, total_mem
-from host_info
-group by cpu_number, id
-order by total_mem DESC;
+SELECT
+    cpu_number,id,total_mem
+FROM host_info
+GROUP BY cpu_number, id
+ORDER BY cpu_number, total_mem DESC;
 
 -- Round of the TimeStamp with 5 minutes interval.
-CREATE FUNCTION RoundTimeStamp(ts timestamp) RETURNS timestamp AS 
-$ $ 
-BEGIN 
-     RETURN date_trunc('hour', ts) + date_part('minute', ts) :: int / 5 * interval '5 min';
+SELECT
+    date_trunc('hour', timestamp) + date_part('minute', timestamp):: int / 5 * interval '5 min'
+FROM host_usage;
+
+
+-- you can also create a function for convenience purposes so your qeury looks cleaner
+CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
+$$
+BEGIN
+    RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
 END;
-$ $ 
-LANGUAGE PLPGSQL;
+$$
+    LANGUAGE PLPGSQL;
 
--- Return Average Memory usage
-SELECT
-    host_id, hostInfo.hostname,
-    RoundTimeStamp(hostUsage.timestamp) AS timestamp,
-    cast(AVG((hostInfo.total_mem - hostUsage.memory_free) * 100 / total_mem) AS int) avg_used_mem_percentage
-from host_info hostInfo
-    inner join host_usage hostUsage on hostInfo.id = hostUsage.host_id
-group by RoundTimeStamp(hostUsage.timestamp), host_id, hostname;
+-- verify round5 function
+SELECT host_id, timestamp, round5(timestamp)
+FROM host_usage;
 
--- Retreiving server details failing to insert 3 rows in a 5 minutes interval.
+--Return average memory usage
 SELECT
-    DISTINCT host_id,
-    RoundTimeStamp(host_usage.timestamp) as timeStamps,
-    COUNT(*) as points
+    host_id, hostname,
+    ROUND((AVG(host_info.total_mem-host_usage.memory_free * 1000)/host_info.total_mem) * 100) AS avg_used_mem_percentage,
+    round5(host_usage.timestamp)
 FROM host_usage
-GROUP BY timeStamps, host_id
-HAVING COUNT(*) <= 3;
+JOIN host_info
+ON host_info.id = host_usage.host_id
+GROUP BY host_usage.timestamp, host_id, hostname, total_mem;
+
+-- Detect host failure
+SELECT DISTINCT
+    host_id,
+    round5(host_usage.timestamp) AS time_stamp,
+    COUNT(*) AS data_points
+FROM host_usage
+GROUP BY time_stamp, host_id HAVING COUNT(*) <= 3;
